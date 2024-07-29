02a52b90920fb2bce03209dd406ad2a482b284dc
jae <ahmadjaelani8685@gmail.com>
2024-07-30 02:33:25 +0700

    First Commit

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button ({text}) {
-    return (
-        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            {text}
-        </button>
-    );
-}
-
-export default Button

9dd3301002d5ce6a094f8051f57f57cb39a9d4dd
jae <ahmadjaelani8685@gmail.com>
2024-07-30 02:24:03 +0700

    First Commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+function Button ({text}) {
+    return (
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            {text}
+        </button>
+    );
+}
+
+export default Button
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
+
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 import './index.css'
+import EditContact, { action as editAction, } from './routes/edit.jsx';
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
